9f6d8df7c264299d7c997ff6d7c94d524e70ea85
fernandog2186 <56263504+fernandog2186@users.noreply.github.com>
2019-10-07 10:31:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Fernando"
+let userName = "Fernandog2186";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
